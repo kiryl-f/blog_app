a81015831b0113ff98a36d9b3d3f6e52f06acb62
kiryl-f <filippovickirill81@gmail.com>
2022-12-31 14:28:42 +0300

    v 0.2 registration/authentication system implemented

--- main_page.php
+++ main_page.php
@@ -1,16 +1,2 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$db_password = "";
-$dbname = "blog_app_db";
-
-$conn = new mysqli($servername, $username, $db_password, $dbname);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-$sql = "SELECT login, password FROM users WHERE login='login'";
-$query = mysqli_query($conn, $sql);
-$arr = mysqli_fetch_all($query, MYSQLI_ASSOC);
-var_dump($arr);
+echo 'main';
