c451de8363e987a0323616764502adb67e2962c1
kiryl-f <filippovickirill81@gmail.com>
2023-01-05 22:33:16 +0300

    v 1.0 started to implement blog app functional

--- new_blog_post.php
+++ new_blog_post.php
@@ -1,1 +1,29 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Registration</title>
+    <link rel="stylesheet" href="css/header.css">
+    <script src="https://code.jquery.com/jquery-3.6.1.js"></script>
+</head>
+
+<body>
+
+<div class="topnav">
+    <a href="index.php">Home</a>
+    <div class="topnav-right">
+        <a href="authentication.php">Log in</a>
+        <a href="registration.php">Create an account</a>
+    </div>
+</div>
+
+<div>
+    <form>
+        <h2>New blog post</h2>
+        <textarea id="blog_name" placeholder="Blog name..."></textarea>
+        <br>
+        <textarea id="blog_text" rows="20" cols="50" placeholder="Blog text"></textarea>
+        <br>
+    </form>
+</div>
+</body>
