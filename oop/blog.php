bd1c20e7e111ed03520fa79324301e49ff8b0e6d
kiryl-f <filippovickirill81@gmail.com>
2023-01-08 23:29:33 +0300

    v 1.0 started to implement blog app functional

---
+++ oop/blog.php
@@ -0,0 +1,46 @@
+<?php
+
+class Blog {
+    private $name;
+    private $text;
+
+    public function __construct($name, $text)
+    {
+        $this->name = $name;
+        $this->text = $text;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * @param mixed $text
+     */
+    public function setText($text): void
+    {
+        $this->text = $text;
+    }
+
+
+}
--- sql/blogs_db_handler.php
+++ sql/blogs_db_handler.php
@@ -1,1 +1,13 @@
 <?php
+
+$conn = new mysqli("localhost", "root", "", 'blog_app_db');
+
+function addBlogToDB($name, $text) {
+    global $conn;
+    $sql = "INSERT INTO blogs (name, text, name, email) VALUES ('$name', '$text')";
+    if (mysqli_query($conn, $sql)) {
+        echo json_encode(array('result' => 'blog_added'));
+    } else {
+        echo json_encode(array('result' => 'error'));
+    }
+}
