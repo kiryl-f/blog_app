2208111c50b484b88910540d4ff42112d92c2e2f
kiryl-f <filippovickirill81@gmail.com>
2023-01-08 23:33:39 +0300

    v 1.0 started to implement blog app functional

--- oop/blog.php
+++ oop/blog.php
@@ -10,33 +10,21 @@ class Blog {
         $this->text = $text;
     }
 
-    /**
-     * @return mixed
-     */
     public function getName()
     {
         return $this->name;
     }
 
-    /**
-     * @param mixed $name
-     */
     public function setName($name): void
     {
         $this->name = $name;
     }
 
-    /**
-     * @return mixed
-     */
     public function getText()
     {
         return $this->text;
     }
 
-    /**
-     * @param mixed $text
-     */
     public function setText($text): void
     {
         $this->text = $text;
