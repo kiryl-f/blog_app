c6474e03b1a1fd88a362f23858f77851221e0607
kiryl-f <filippovickirill81@gmail.com>
2023-01-09 00:43:55 +0300

    v 1.0 started to implement blog app functional

--- add_blog_post_to_db.php
+++ add_blog_post_to_db.php
@@ -1,3 +1,14 @@
 <?php
+
+require_once 'oop/blog.php';
+
 $name = $_POST['blog_name'];
 $text = $_POST['blog_text'];
+$blog = new Blog($name, $text);
+
+if($blog->getErrorMessage() === '') {
+    //add to DB
+    echo json_encode(array('res' => 'added'));
+} else {
+    echo json_encode(array('res' => $blog->getErrorMessage()));
+}
--- oop/blog.php
+++ oop/blog.php
@@ -30,10 +30,23 @@ class Blog {
         $this->text = $text;
     }
 
-    public function checkLength() {
+    public function checkLength(): string {
         if(strlen($this->text) < 20) {
-            return 'Text must be at least 20 characters long';
+            return "Text must be at least 20 characters long\n";
         }
         return '';
+    }
+
+    public function checkName(): string {
+        if(strlen($this->name) < 5) {
+            return "Text must be at least 20 characters long\n";
+        }
+        return '';
+    }
+
+    public function getErrorMessage(): string {
+        $error_message = $this->checkName();
+        $error_message .= $this->checkLength();
+        return $error_message;
     }
 }
