0f404da5e5d941728253e41be9abf762d9f1b506
kiryl-f <filippovickirill81@gmail.com>
2023-01-10 00:00:48 +0300

    v 1.0 started to implement blog app functional

--- sql/blogs_db_handler.php
+++ sql/blogs_db_handler.php
@@ -4,7 +4,8 @@ $conn = new mysqli("localhost", "root", "", 'blog_app_db');
 
 function addBlogToDB($name, $text) {
     global $conn;
-    $sql = "INSERT INTO blogs (name, text, name, email) VALUES ('$name', '$text')";
+    $date = date("Y/m/d");
+    $sql = "INSERT INTO blogs (name, text, name, email) VALUES ('$name', '$text', '$date')";
     if (mysqli_query($conn, $sql)) {
         echo json_encode(array('result' => 'blog_added'));
     } else {
