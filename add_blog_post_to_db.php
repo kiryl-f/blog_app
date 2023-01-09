b86f1f7de57c6bedef82cebd1b5abc0cb56c0753
kiryl-f <filippovickirill81@gmail.com>
2023-01-09 23:59:26 +0300

    v 1.0 started to implement blog app functional

--- add_blog_post_to_db.php
+++ add_blog_post_to_db.php
@@ -8,6 +8,13 @@ $blog = new Blog($name, $text);
 
 if($blog->getErrorMessage() === '') {
     //add to DB
+    $sql = "INSERT INTO users ($name, $text) VALUES ('$name',   '$text','$blog->getDate()')";
+    if (mysqli_query($conn, $sql)) {
+        setcookie('name', $name);
+        echo json_encode(array('result' => 'user_created'));
+    } else {
+        echo json_encode(array('result' => 'error'));
+    }
     echo json_encode(array('res' => 'added'));
 } else {
     echo json_encode(array('res' => $blog->getErrorMessage()));
