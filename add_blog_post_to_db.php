507a3b06c26695b4897e1e6f188145ebdb68b1da
kiryl-f <filippovickirill81@gmail.com>
2023-02-09 00:25:36 +0300

    implemented blog editing

--- add_blog_post_to_db.php
+++ add_blog_post_to_db.php
@@ -3,14 +3,16 @@
 require_once 'oop/blog.php';
 require_once 'blogs_db_handler.php';
 
-$name = $_POST['blog_name'];
-$text = $_POST['blog_text'];
-$blog = new Blog($name, $text);
+if(!empty($_SERVER['HTTP_X_REQUESTED_WITH'])  && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+    $name = $_POST['blog_name'];
+    $text = $_POST['blog_text'];
+    $blog = new Blog($name, $text);
 
-if($blog->getErrorMessage() === '') {
-    $res = addBlogToDB($name, $text);
-    echo json_encode(array('result' => $res));
-} else {
-    echo json_encode(array('result' => $blog->getErrorMessage()));
+    if($blog->getErrorMessage() === '') {
+        $res = addBlogToDB($name, $text);
+        echo json_encode(array('result' => $res));
+    } else {
+        echo json_encode(array('result' => $blog->getErrorMessage()));
+    }
+
 }
-//echo json_encode(array('res' => 'cool'));
